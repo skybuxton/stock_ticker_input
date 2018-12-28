17f3e24d97824dc5bcaa9d85f709db4d521c8198
Navarra Sky Buxton <navarrabuxton@gmail.com>
2018-12-28 11:04:11 -0500

    added route

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# functions
+def get_stock_ticker_data(symbol):
+	return "my symbol is %s" % symbol
+
+# Routes
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -13,6 +18,10 @@ def about():
 @app.route('/secret')
 def secret():
 	return "this is a secret"
+
+@app.route('/myticker/<symbol>')
+def myticker():
+	return get_stock_ticker_data(symbol)
 
 if __name__ == '__main__':
   app.run(port=33507)
