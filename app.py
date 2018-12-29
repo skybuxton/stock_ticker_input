161d694320901c394722567b74b6110c3f00954b
Navarra Sky Buxton <navarrabuxton@gmail.com>
2018-12-28 23:03:06 -0500

    cleanup

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import quandl
 
 app = Flask(__name__)
 
-# functions
-def get_stock_ticker_data(symbol):
-	return "my max price: %s\n\nmy min price:%s" % (quandl.get_high(symbol), quandl.get_low(symbol))
-
 # Routes
 @app.route('/')
 def index():
@@ -25,7 +21,6 @@ def ticker(symbol):
 	high = quandl.get_high(symbol)
 	low  = quandl.get_low(symbol)
 	return render_template('ticker.html', ticker=symbol, high=high, low=low)
-	# return get_stock_ticker_data(symbol)
 
 if __name__ == '__main__':
   app.run(port=33507)
